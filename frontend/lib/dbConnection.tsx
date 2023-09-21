156521b433c10f24dd1df3f2d81c5b7a00b26d99
Ferran Campos <ferran98campos@gmail.com>
2023-09-22 01:26:13 +0200

    API Handler refactor
    Now it extends from RequestHandler and users has its own service with functions such as InsertOne() and getAll()

--- frontend/lib/dbConnection.tsx
+++ frontend/lib/dbConnection.tsx
@@ -1,54 +1,27 @@
-import _mongoose, { connect } from "mongoose";
+import mongoose, { Connection } from 'mongoose';
 
-declare global {
-  var mongoose: {
-    promise: ReturnType<typeof connect> | null;
-    conn: typeof _mongoose | null;
-  };
-}
+let cachedConnection: Connection | null = null;
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
-
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections from growing exponentially
- * during API Route usage.
- */
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
+export default async function dbConnect(): Promise<Connection> {
+  if (cachedConnection) {
+    return cachedConnection;
   }
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
+  const MONGODB_URI = process.env.MONGODB_URI;
 
-    cached.promise = connect(MONGODB_URI!, opts).then((mongoose) => {
-      return mongoose;
-    });
+  if (!MONGODB_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
   }
 
   try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    throw e;
+    const opts: mongoose.ConnectOptions = {
+      bufferCommands: false
+    };
+
+    const connection = await mongoose.connect(MONGODB_URI, opts);
+    cachedConnection = connection.connection; 
+    return cachedConnection;
+  } catch (error) {
+    throw error;
   }
-
-  return cached.conn;
 }
-
-export default dbConnect;
