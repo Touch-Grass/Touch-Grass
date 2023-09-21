1bc43a485af5ba0dffaf6c790856af18765066be
Christine Qiu <cqiu@kth.se>
2023-09-21 16:06:36 +0200

    finished signin/signup UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,39 @@
-import styles from './page.module.css'
+'use client'
+import React, { useState } from 'react';
+import styles from './page.module.css';
+import Signin from '../components/signin';
 
 export default function Home() {
+  const [isSignInVisible, setIsSignInVisible] = useState(true);
+  const [signInStep, setSignInStep] = useState(0);
+  const openSignIn = () => {
+    // setIsSignInVisible(true);
+    setSignInStep(1);
+  };
+  const closeSignIn = () => {
+    // setIsSignInVisible(false);
+  };
   return (
     <body>
-    <nav className='navbar'> <img src='../../public/logo-no-dots.svg'></img><header>Touch Grass</header><div className='menu'><button>Sign in</button></div></nav>
-    <main>
+    <nav className='navbar'> 
+      <img src='../../public/logo-no-dots.svg'></img>
+      <header>TouchGrass</header>
+      <div className='menu'>
+        <button className={styles.signinButton} onClick={openSignIn}>Sign in/Sign up</button>
+      </div>
+    </nav>
+    <main onClick={closeSignIn}>
       <input type='text' placeholder='Where is the grass?' id={styles.search}></input>
+      {
+        isSignInVisible 
+        && <Signin 
+              isVisible={isSignInVisible} 
+              signInStep={signInStep} 
+              onUsernameEmail={()=>{setSignInStep(2)}}
+              onPassword={()=>{setSignInStep(3)}}
+              onSignup={()=>{setSignInStep(0)}}
+            />
+      }
     </main>
     </body>
   )
