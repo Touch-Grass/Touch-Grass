40d38bb7cd14ddffec7bfe09970050cb5735a854
malinjam <malin.marques@gmail.com>
2023-09-25 10:07:40 +0200

    frontend stuff (WIP)

--- frontend/src/app/my-trail-list/page.tsx
+++ frontend/src/app/my-trail-list/page.tsx
@@ -1,17 +1,24 @@
 export default function MyTrails() {
   return (
     <body>
-    <nav className='navbar'> 
-    <img src='../app/logo-no-dots.svg'/>
-    <header>Touch Grass</header>
-    <div className='menu'><button>
-      Sign in</button></div>
-    </nav>
-    <main>
-      <div className='trail-container'>
-        <div className='trail-list-heading'><h1>My Trails</h1> </div>
-         </div>
-    </main>
+      <nav className="navbar">
+        <img src="../app/logo-no-dots.svg" />
+        <header>Touch Grass</header>
+        <div className="menu">
+          <button>Sign in</button>
+        </div>
+      </nav>
+      <main>
+        <div className="trail-list-container">
+          <div className="trail-list-heading">
+            <h1>My Trails</h1>{" "}
+          </div>
+          <div className="trail-container">
+            <img src="../../../public/minecraft-grass.png" />
+            <div className="trail-text"></div>
+          </div>
+        </div>
+      </main>
     </body>
-  )
+  );
 }
