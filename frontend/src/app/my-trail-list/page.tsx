a6c92fe4423714dce742947f80d151eb1c12c338
malinjam <malin.marques@gmail.com>
2023-09-25 13:31:48 +0200

    Update page.tsx

--- frontend/src/app/my-trail-list/page.tsx
+++ frontend/src/app/my-trail-list/page.tsx
@@ -11,11 +11,13 @@ export default function MyTrails() {
       <main>
         <div className="trail-list-container">
           <div className="trail-list-heading">
-            <h1>My Trails</h1>{" "}
+            <h1>My Trails</h1>
           </div>
           <div className="trail-container">
-            <img src="../../../public/minecraft-grass.png" />
-            <div className="trail-text"></div>
+            <img src="../../../public/minecraft-grass.png" /> 
+            <div className="trail-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec mollis enim. Integer nunc nunc, ullamcorper a ante in, tincidunt congue neque. Nunc mollis tincidunt tortor sollicitudin lobortis. Nam lorem diam, pellentesque at sodales vitae, sagittis vel tortor. Curabitur vitae consectetur lectus, ut congue urna. Fusce vehicula enim eget lorem varius, sit amet egestas orci mollis. Suspendisse vel massa sapien. Pellentesque quis ornare massa, ac viverra elit. Cras venenatis at odio quis lobortis. Quisque laoreet efficitur est quis sagittis. Vivamus tincidunt nibh volutpat lorem condimentum ullamcorper. 
+            </div>
           </div>
         </div>
       </main>
