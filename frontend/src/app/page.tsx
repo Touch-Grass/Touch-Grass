762ff20ffadb7e3009f7c7f5313ca6f6b34ab05f
malinjam <malin.marques@gmail.com>
2023-10-03 00:16:32 +0200

    done

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -18,8 +18,9 @@ export default function Home() {
     <body>
       <main>
         <div className="home-command">You should <a href="https://youtu.be/JSnUF4d9CBk?si=gHwIQvNeDnCqP77d" className='home-link'>touch grass</a> today.</div>
-        <div className='home-container' id='home-search-width'><input type='text' placeholder='Where do you want to go?' className='search-bar' id='home-search-bar'/>
+        <div className='home-container'><input type='text' placeholder='Where do you want to go?' className='search-bar' id='home-search-bar'/>
         <button className='home-search-button'>Search</button></div>
+        <div className='home-command-bottom'>Scroll down to discover</div>
 
         {
           isSignInVisible 
