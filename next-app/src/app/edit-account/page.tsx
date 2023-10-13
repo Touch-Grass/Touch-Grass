000ba7bff824ca5399472ec084b4627661ac2890
Christine Qiu <cqiu@kth.se>
2023-10-13 15:24:50 +0200

    Try to fix props

--- next-app/src/app/edit-account/page.tsx
+++ next-app/src/app/edit-account/page.tsx
@@ -8,48 +8,50 @@ interface EditAccountProps {
   onProfilePicClicked: () => void;
 }
 
-export default function EditAccount(props: EditAccountProps) {
+const EditAccount: React.FC<EditAccountProps> = props => {
   const [isUserMenuVisible, setIsUserMenuVisible] = useState(false);
-  const [signInStep, setSignInStep] = useState(0);
-    return (
-      <body>
-        <Navbar
-          signInStep={signInStep}
-          isUserMenuVisible={isUserMenuVisible}
-          onOpenSignIn={()=>setSignInStep(1)}
-          onOpenUserMenu={()=>setIsUserMenuVisible(true)}
-          onLogOut={()=>{setSignInStep(0);setIsUserMenuVisible(false);}}
-          onInputUsernameEmail={()=>setSignInStep(2)}
-          onInputPassword={()=>setSignInStep(3)}
-          onSignup={()=>setSignInStep(4)}
-        />
-        <main className="profile-page-container">
-            <div className='profile-pic-container' onClick={props.onProfilePicClicked}>
-              <ImageUpload imageUrl='/userIcon.png'></ImageUpload>
+  const [signInStep, setSignInStep] = useState(0); 
+  return (
+    <body>
+      <Navbar
+        signInStep={signInStep}
+        isUserMenuVisible={isUserMenuVisible}
+        onOpenSignIn={()=>setSignInStep(1)}
+        onOpenUserMenu={()=>setIsUserMenuVisible(true)}
+        onLogOut={()=>{setSignInStep(0);setIsUserMenuVisible(false);}}
+        onInputUsernameEmail={()=>setSignInStep(2)}
+        onInputPassword={()=>setSignInStep(3)}
+        onSignup={()=>setSignInStep(4)}
+      />
+      <main className="profile-page-container">
+          <div className='profile-pic-container' onClick={props.onProfilePicClicked}>
+            <ImageUpload imageUrl='/userIcon.png'></ImageUpload>
+          </div>
+          <div className='profile-info-container'>
+            <div className='profile-entry-container'>
+                <div className='profile-text'>Username</div>
+                <input className='profile-input' type='text' placeholder="Chris"></input>
             </div>
-            <div className='profile-info-container'>
-              <div className='profile-entry-container'>
-                  <div className='profile-text'>Username</div>
-                  <input className='profile-input' type='text' placeholder="Chris"></input>
-              </div>
-              <div className='profile-entry-container'>
-                <div className='profile-text'>Email address</div>
-                <input className='profile-input' type='text'  placeholder="chris@mail.com"></input>
-              </div>
-              <div className='profile-entry-container'>
-                <div className='profile-text'>Password</div>
-                <input className='profile-input' type='password'></input>
-              </div>
-              <div className='profile-entry-container'>
-                <div className='profile-text'>Password confirmation</div>
-                <input className='profile-input' type='password'></input>
-              </div>
-              <div className='profile-button-container'>
-                <button className='profile-button' id='profile-save-button'>Save</button>
-                <button className='profile-button' id='profile-delete-button'>Delete account</button>
-              </div>
+            <div className='profile-entry-container'>
+              <div className='profile-text'>Email address</div>
+              <input className='profile-input' type='text'  placeholder="chris@mail.com"></input>
             </div>
-        </main>
-      </body>
-    );
-  }
+            <div className='profile-entry-container'>
+              <div className='profile-text'>Password</div>
+              <input className='profile-input' type='password'></input>
+            </div>
+            <div className='profile-entry-container'>
+              <div className='profile-text'>Password confirmation</div>
+              <input className='profile-input' type='password'></input>
+            </div>
+            <div className='profile-button-container'>
+              <button className='profile-button' id='profile-save-button'>Save</button>
+              <button className='profile-button' id='profile-delete-button'>Delete account</button>
+            </div>
+          </div>
+      </main>
+    </body>
+  );
+};
+
+export default EditAccount;
