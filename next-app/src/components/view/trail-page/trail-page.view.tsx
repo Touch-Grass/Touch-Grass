9266e9cc41562319c5add74ed15d44867fa01341
malinjam <malin.marques@gmail.com>
2023-10-14 19:56:10 +0200

    wip

--- next-app/src/components/view/trail-page/trail-page.view.tsx
+++ next-app/src/components/view/trail-page/trail-page.view.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {ServerTrailWithID} from "@/models/server/trail/trail";
+import { ServerTrailWithID } from "@/models/server/trail/trail";
 import "./trail-page.view.scss";
 
 interface TrailPageViewProps {
     trail: ServerTrailWithID;
 }
 
-const TrailPageView: React.FC<TrailPageViewProps> = async props => {
+const TrailPageView: React.FC<TrailPageViewProps> = async (props) => {
     const trail = props.trail;
 
     return (
@@ -14,34 +14,41 @@ const TrailPageView: React.FC<TrailPageViewProps> = async props => {
             <div className={"trail-page-boxed-container"}>
                 {/* TODO: Put trail-header into seperate component*/}
                 <div className={"trail-header"}>
-                    <div className={"trail-header-label"}>
-                        {trail.name}
-                    </div>
+                    <div className={"trail-header-label"}>{trail.name}</div>
                 </div>
                 <div className={"trail-page-split"}>
                     <div className={"trail-page-left"}>
                         <div className={"trail-page-basic-properties"}>
                             <div className={"trail-page-prop"}>
-                                <div className={"trail-page-prop-label"}>Length</div>
-                                <div className={"trail-page-prop-value"}>{trail.length.toFixed(1)} km</div>
+                                <div className={"trail-page-prop-label"}>
+                                    Length
+                                </div>
+                                <div className={"trail-page-prop-value"}>
+                                    {trail.length.toFixed(1)} km
+                                </div>
                             </div>
                             <div className={"trail-page-prop"}>
-                                <div className={"trail-page-prop-label"}>Terrain</div>
-                                <div className={"trail-page-prop-value"}>{trail.terrain}</div>
+                                <div className={"trail-page-prop-label"}>
+                                    Terrain
+                                </div>
+                                <div className={"trail-page-prop-value"}>
+                                    {trail.terrain}
+                                </div>
                             </div>
                         </div>
-                        <div className={"trail-page-description"}>{trail.description}</div>
+                        <div className={"trail-page-description"}>
+                            {trail.description}
+                        </div>
                     </div>
                 </div>
             </div>
-<div className="trail-page-comments>
-<h1>Comments</h1>
-{comments.map((comment) => (
-                    <CommentComponent key={comment._id.toString()} comment={comment} />
+            <div className="trail-page-comments">
+                <h1>Comments</h1>
+                {comments.map((comment) => (
+                    <CommentComponent key={comment._id.toString()}comment={comment}/>
                 ))}
- </div>
+            </div>
         </div>
     );
 };
-
 export default TrailPageView;
--- next-app/src/models/server/comment/comment.ts
+++ next-app/src/models/server/comment/comment.ts
@@ -2,7 +2,7 @@ import type {Ref} from "@typegoose/typegoose";
 import {getModelForClass, prop, ReturnModelType} from "@typegoose/typegoose";
 import {IComment} from "@/models/shared/comment/comment.interface";
 import {Mutable, WithID} from "@/models/shared/utility.types";
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 import { Trail } from "../trail/trail";
 import { User } from "../user/user";
 
@@ -19,13 +19,14 @@ export class Comment implements Mutable<ServerComment>{
     date: number;
     @prop()
     trail: Ref<Trail>;
-/*                      TODO: solve return error
+
     public static async addComment(comment: ServerComment): Promise<Document> {
         const record = new CommentModel({...comment});
         return await record.save();
-    }*/
+    }
 }
 
+
 type CommentModelType = ReturnModelType<typeof Comment>;
-export const CommentModel = mongoose.models.Comment as CommentModelType ?? getModelForClass(Comment);
+export const CommentModel: CommentModelType = mongoose.models.Comment as CommentModelType ?? getModelForClass(Comment);
 
