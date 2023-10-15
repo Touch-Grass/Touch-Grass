ed7a0de28bed1cd90e40f2ee86706496b7f7c8b2
Christine Qiu <cqiu@kth.se>
2023-10-15 20:43:27 +0200

    Add signin test

--- next-app/cypress/e2e/register.cy.ts
+++ next-app/cypress/e2e/register.cy.ts
@@ -1,21 +1,22 @@
-describe("Register Functionalities", () => {
+describe("Register and Login Functionalities", () => {
+    let name: string = "John";
+    let surname: string = "Doe";
+    let email: string = `email${Date.now()}@example.com`;
+    let username: string = `username${Date.now()}`;
+    let password: string = "testPassword111*";
     beforeEach(() => {
         cy.visit("http://localhost:3000");
     });
 
-    it("sign in button should be rendered", () => {
-        cy.get(".button-signin");
-    });
-
-    it("should be able to perform new user registeration", () => {
+    it("sign up should be able to perform new user registeration", () => {
         cy.get(".button-signin").click();
         cy.url().should("include", "/register");
-        cy.contains("Sign in.");
-        const name = "John";
-        const surname = "Doe";
-        const email = `email${Date.now()}@example.com`;
-        const username = `username${Date.now()}`;
-        const password = "testPassword111*";
+        cy.contains("Sign up.");
+        name = "John";
+        surname = "Doe";
+        email = `email${Date.now()}@example.com`;
+        username = `username${Date.now()}`;
+        password = "testPassword111*";
         cy.get('input[name="name"]').then(($input) => {
             cy.wrap($input).type(name);
         });
@@ -37,12 +38,30 @@ describe("Register Functionalities", () => {
         cy.get(".button-default").should("have.text", "Register").click();
         cy.contains("Registration completed!");
         cy.get(".button-default").should("have.text", "Log in").click();
+        cy.url().should("include", "/login");
+        cy.contains("Log in.");
         cy.get('input[name="username"]').then(($input) => {
             cy.wrap($input).type(username);
         });
         cy.get('input[name="password"]').then(($input) => {
             cy.wrap($input).type(password);
         });
+        cy.get(".button-default").should("have.text", "Log in").click();
+        cy.contains("Hello there");
+        cy.contains(username);
+    });
+
+    it("log in should work", () => {
+        cy.get(".button-login:first").click();
+        cy.url().should("include", "/login");
+        cy.contains("Log in.");
+        cy.get('input[name="username"]').then(($input) => {
+            cy.wrap($input).type(username);
+        });
+        cy.get('input[name="password"]').then(($input) => {
+            cy.wrap($input).type(password);
+        });
+
         cy.get(".button-default").should("have.text", "Log in").click();
         cy.contains("Hello there");
         cy.contains(username);
