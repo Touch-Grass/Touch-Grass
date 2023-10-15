d27f7e67d189c3fffe7e6cd63267497e21092d2b
Christine Qiu <cqiu@kth.se>
2023-10-15 17:08:43 +0200

    Add search test

---
+++ next-app/cypress/e2e/search.cy.ts
@@ -0,0 +1,16 @@
+describe("Search Bar Functionalities", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
+    });
+
+    it("should be rendered", () => {
+        cy.get("input").should("have.class", "hero-search-bar");
+    });
+
+    it("should perform a search", () => {
+        cy.get("input").type("sandhamn");
+        cy.get(".hero-search-button").click();
+        cy.url().should("include", "search?l=sandhamn");
+        cy.contains("trails in sandhamn");
+    });
+});
