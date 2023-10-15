ebda1c6b3b47ea2998be98ad071e0678fb60428e
Christine Qiu <cqiu@kth.se>
2023-10-15 23:05:48 +0200

    Modify add-trail test

--- next-app/cypress/e2e/register-signin-addTrail-logout.cy.ts
+++ next-app/cypress/e2e/register-signin-addTrail-logout.cy.ts
@@ -1,4 +1,6 @@
-describe("Register, login, logout functionalities", () => {
+import "cypress-file-upload";
+
+describe("Register, login, add-trail, logout functionalities", () => {
     let name: string = "John";
     let surname: string = "Doe";
     let email: string = `email${Date.now()}@example.com`;
@@ -8,7 +10,7 @@ describe("Register, login, logout functionalities", () => {
         cy.visit("http://localhost:3000");
     });
 
-    it("sign up should be able to perform new user registeration", () => {
+    it("should be able to perform new user registeration", () => {
         cy.get(".button-signin").click();
         cy.url().should("include", "/register");
         cy.contains("Sign up.");
@@ -31,21 +33,47 @@ describe("Register, login, logout functionalities", () => {
         cy.get('input[name="username"]').type(username);
         cy.get('input[name="password"]').type(password);
         cy.get(".button-default").should("have.text", "Log in").click();
-        cy.contains("Hello there");
-        cy.contains(username);
+        cy.contains(name);
     });
 
-    it("log in and log out should work", () => {
+    it("should be able to add a new trail after logged in", () => {
+        let trailName: string = "Hunter's Footprint";
+        let location: string = "Runmaro";
+        let description: string =
+            "You'll probably see a deer or two on the way.";
+        let terrain: string = "Forest";
+        let difficulty: string = "Medium";
+        let imageUrl: string = "/img/trail-image.jpg";
         cy.get(".button-login:first").click();
         cy.url().should("include", "/login");
         cy.contains("Log in.");
         cy.get('input[name="username"]').type(username);
         cy.get('input[name="password"]').type(password);
-
         cy.get(".button-default").should("have.text", "Log in").click();
-        cy.contains("Hello there");
-        cy.contains(username);
-        cy.get(".user-icon-chevrown-down").click();
+        cy.contains(name);
+        cy.contains("New trail").click();
+        cy.get('input[name="name"]').type(trailName);
+        cy.get('input[name="location"]').type(location);
+        cy.get('textarea[name="description"]').type(description);
+        cy.get('select[name="terrain"]').select(terrain);
+        cy.get('select[name="difficulty"]').select(difficulty);
+        cy.get('input[type="file"]').attachFile(imageUrl);
+        cy.get(".trail-creator-view").click("center");
+        cy.get(".trail-creator-view").click("right");
+        cy.contains("Submit").click();
+        cy.url().should("include", "/trail");
+        cy.contains("Length");
+        cy.contains(description);
+    });
+
+    it("should be able to log out", () => {
+        cy.get(".button-login:first").click();
+        cy.url().should("include", "/login");
+        cy.contains("Log in.");
+        cy.get('input[name="username"]').type(username);
+        cy.get('input[name="password"]').type(password);
+        cy.get(".button-default").should("have.text", "Log in").click();
+        cy.contains(name).click();
         cy.contains("Log Out").click();
         cy.get(".button-signin");
         cy.get(".button-login");
