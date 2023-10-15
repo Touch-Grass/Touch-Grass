50db2585e0c82d70704b4d83cbab4c556a4a62d3
Markus Wagner <prttmprpht@gmail.com>
2023-10-16 00:08:57 +0200

    Fix server start script

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Build without cache
-echo "Building with --no-cache..."
-docker-compose build --no-cache
-
 # Start services
 echo "Starting docker-compose services..."
-docker-compose up
+docker-compose up --build
