a19657cc40398d99eee94ab664c8344ac2cb2ba6
Christine Qiu <cqiu@kth.se>
2023-10-15 20:55:56 +0200

    Update search test

--- next-app/cypress/e2e/search.cy.ts
+++ next-app/cypress/e2e/search.cy.ts
@@ -7,12 +7,17 @@ describe("Search Bar Functionalities", () => {
         cy.get("input").should("have.class", "hero-search-bar").click();
     });
 
-    it("should perform a search", () => {
+    it("should be able to perform searches both from home page and search page", () => {
         cy.get("input").type("sandhamn");
         cy.get(".hero-search-button").click();
         cy.url().should("include", "search?l=sandhamn");
         cy.contains("trails in sandhamn");
+        cy.get("input")
+            .should("have.class", "search-page-input")
+            .clear()
+            .type("Stockholm");
+        cy.get(".button-search").click();
+        cy.url().should("include", "search?l=Stockholm");
+        cy.contains("trails in Stockholm");
     });
-
-    // TODO: should also be able to search from search page.
 });
