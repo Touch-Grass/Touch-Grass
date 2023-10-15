fecab2efea353ddf9bd05d84c16d2e5227951ac0
Ferran Campos <ferran98campos@gmail.com>
2023-10-16 01:45:44 +0200

    Hotfix of comment view

--- next-app/pages/api/comment/index.ts
+++ next-app/pages/api/comment/index.ts
@@ -6,7 +6,9 @@ import { HttpStatus, sendCustomError } from "@/utils/HTTPError/HTTPErrorUtils";
 import { IComment } from "@/models/shared/comment/comment.interface";
 import { CommentsService } from "@/services/comments/comments.service";
 import { Comment } from "@/models/server/comment/comment";
-
+import { TrailsService } from "@/services/trails/trails.service";
+import { Ref } from "@typegoose/typegoose";
+import { Trail } from "@/models/server/trail/trail";
 class CommentHandler extends RequestHandler {
     constructor() {
         super(); // Call the constructor of the parent class
@@ -36,7 +38,10 @@ class CommentHandler extends RequestHandler {
             let comment = new Comment(request.body.comment as IComment);
             //Set creation date
             comment.date = Date.now();
-            comment.trail=request.body.trail;
+
+            //const trail = TrailsService.findById()
+            const trail = await TrailsService.findById(request.body.trail._id);
+            comment.trail= trail as Ref<Trail>;
 
             //Find comment creator reference based on provided token
             const user = await AuthService.getUserInfoFromToken(token);
