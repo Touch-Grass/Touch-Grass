016a9421fbf9107bd64ae054ea10eba0b6f2cdb8
malinjam <malin.marques@gmail.com>
2023-10-16 14:03:34 +0200

    dates will look same bc the random numbers r 2 lo

--- next-app/src/components/view/comment/comment.tsx
+++ next-app/src/components/view/comment/comment.tsx
@@ -5,6 +5,7 @@ import { ServerCommentWithID } from "@/models/server/comment/comment";
 import { User } from "@/models/server/user/user";
 import type {Ref} from "@typegoose/typegoose";
 import UserRepresentationView from "../user-representation/user-representation.view";
+import moment from "moment";
 
 interface Commentprops {
     comment:ServerCommentWithID;
@@ -18,7 +19,7 @@ const CommentComponent: React.FC<Commentprops> = (props) => {
         <div className="comment-container">
         <div className="comment-title">{comment.title}</div>
         <div className="comment-text">{comment.text}</div>
-        <div className="comment-user"> <UserRepresentationView userName={user.username}></UserRepresentationView></div>
+        <div className="comment-user-date"> <UserRepresentationView userName={user.username}></UserRepresentationView> <div>{moment(comment.date).format('DD/MM/YYYY')}</div></div>
         </div>
         </>
     );
