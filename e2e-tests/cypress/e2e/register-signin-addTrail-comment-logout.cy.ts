7690185f46e6085057ed29c9d3093ba3b15e9a81
Christine Qiu <cqiu@kth.se>
2023-10-16 14:29:06 +0200

    Modify add-trail test

--- e2e-tests/cypress/e2e/register-signin-addTrail-comment-logout.cy.ts
+++ e2e-tests/cypress/e2e/register-signin-addTrail-comment-logout.cy.ts
@@ -57,32 +57,42 @@ describe("Register, login, add-trail, logout functionalities", () => {
         cy.get('select[name="terrain"]').select(terrain);
         cy.get('select[name="difficulty"]').select(difficulty);
         cy.get('input[type="file"]').attachFile(imageUrl);
-        cy.get(".trail-creator-view")
-            .click(550, 150)
-            .click(650, 400)
-            .click(200, 400)
-            .click(500, 250)
-            .click(450, 650)
-            .click(650, 300)
-            .click(500, 700)
-            .click(400, 250);
-        cy.get(".add-trail-page-creator-stats").within(() => {
-            cy.contains("Current Length")
-                .next()
-                .invoke("text")
-                .then((lengthText) => {
-                    const length = lengthText.trim();
-                    cy.log(`Length: ${length}`);
-                });
 
-            cy.contains("Current Duration")
-                .next()
-                .invoke("text")
-                .then((durationText) => {
-                    const duration = durationText.trim();
-                    cy.log(`Duration: ${duration}`);
-                });
+        cy.get(".trail-creator-view").then(($element) => {
+            const width = $element.width();
+            const height = $element.height();
+
+            const relativeX1 = width * 0.3;
+            const relativeY1 = height * 0.3;
+            const relativeX2 = width * 0.6;
+            const relativeY2 = height * 0.6;
+            const relativeX3 = width * 0.9;
+            const relativeY3 = height * 0.9;
+
+            cy.wrap($element).click(relativeX1, relativeY1);
+            cy.wrap($element).click(relativeX2, relativeY2);
+            cy.wrap($element).click(relativeX3, relativeY3);
         });
+        // .then(() => {
+        //     cy.get(".add-trail-page-creator-stats").within(() => {
+        //         cy.contains("Current Length")
+        //             .next()
+        //             .invoke("text")
+        //             .then((lengthText) => {
+        //                 const length = lengthText.split("km")[0].trim();
+        //                 cy.wrap(length).should("not.eq", "0.0");
+        //             });
+
+        //         cy.contains("Current Duration")
+        //             .next()
+        //             .invoke("text")
+        //             .then((durationText) => {
+        //                 const duration = durationText.trim();
+        //                 cy.wrap(duration).should("not.eq", "0h0m");
+        //             });
+        //     });
+        // });
+
         cy.contains("Submit").click();
         cy.url().should("include", "/trail");
         cy.contains("Length");
@@ -102,7 +112,7 @@ describe("Register, login, add-trail, logout functionalities", () => {
         cy.contains("Comments");
         cy.get('input[type="text"][name="title"]').type(commentTitle);
         cy.get(
-            'input.add-comment-main-text[placeholder="What do you think about the trail?"]'
+            'textarea.add-comment-main-text[placeholder="What do you think about the trail?"]'
         ).type(commentText);
         cy.get('button.button-default[title="submit"]').click();
         cy.get("div.comment-title").contains(commentTitle);
